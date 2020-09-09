516c35eb2cbc5cd3bfc7c2d51bf8a1d1bba3802a
Chris Kubick <socalledsound@gmail.com>
2020-09-08 17:38:02 -0700

    multiple ball objects

--- index.js
+++ index.js
@@ -5,9 +5,41 @@ const ball1 = {
     x: 300,
     y: 300,
     r: 100,
-    fillColor: [0,220,20],
-    strokeColor: [0, 220, 20]
-}
+    speed: 1,
+    fillColor: [190,80,230],
+    strokeColor: [0,220,20],
+    outlineWidth: 6,
+    rightSound: sounds[0],
+    leftSound: sounds[1],
+    soundLength: 2000,
+} 
+
+const ball2 = {
+    x: 300,
+    y: 100,
+    r: 50,
+    speed: 2,
+    fillColor: [190,80,230],
+    strokeColor: [0,220,20],
+    outlineWidth: 6,
+    rightSound: sounds[2],
+    leftSound: sounds[3],
+    soundLength: 1000,
+} 
+
+const ball3 = {
+    x: 300,
+    y: 200,
+    r: 80,
+    speed: 2,
+    fillColor: [190,80,230],
+    strokeColor: [0,220,20],
+    outlineWidth: 6,
+    rightSound: sounds[4],
+    leftSound: sounds[5],
+    soundLength: 500,
+} 
+
 
 
 function preload(){
@@ -29,9 +61,11 @@ function setup(){
 
 
 function draw(){
-
     drawCircle(ball1);
+    drawCircle(ball2);
+    drawCircle(ball3);
 }
+
 
 const drawCircle = ({x, y, r, strokeColor, fillColor}) => {
         stroke(strokeColor);
