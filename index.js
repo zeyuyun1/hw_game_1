0552d0db8bd3e5e8c05323b80c1be6763cfbda1b
Chris Kubick <socalledsound@gmail.com>
2020-09-24 18:08:06 -0700

    reversing direction finished

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const sounds = Array.from({ length: 6 });
 const ball1 = {
     x: 300,
     y: 300,
-    r: 100,
+    size: 100,
     speed: 1,
     fillColor: [190,80,230],
     strokeColor: [0,220,20],
-    outlineWidth: 6,
+    ballStrokeWeight: 2,
     rightSound: sounds[0],
     leftSound: sounds[1],
     soundLength: 2000,
@@ -23,11 +23,11 @@ const ball1 = {
 const ball2 = {
     x: 300,
     y: 100,
-    r: 50,
+    size: 50,
     speed: 2,
     fillColor: [190,80,230],
     strokeColor: [0,220,20],
-    outlineWidth: 6,
+    ballStrokeWeight: 2,
     rightSound: sounds[2],
     leftSound: sounds[3],
     soundLength: 1000,
@@ -36,20 +36,20 @@ const ball2 = {
 const ball3 = {
     x: 300,
     y: 200,
-    r: 80,
+    size: 80,
     speed: 2,
     fillColor: [190,80,230],
     strokeColor: [0,220,20],
-    outlineWidth: 6,
+    ballStrokeWeight: 2,
     rightSound: sounds[4],
     leftSound: sounds[5],
     soundLength: 500,
 } 
 
 const leftEdge = {
-    x1: 230,
+    x1: 110,
     y1: 0,
-    x2: 230,
+    x2: 110,
     y2: 600,
     color: lineColor,
     width: lineWidth,
@@ -57,9 +57,9 @@ const leftEdge = {
 }
 
 const rightEdge = {
-    x1: 370,
+    x1: 470,
     y1: 0,
-    x2: 370,
+    x2: 470,
     y2: 600,
     color: lineColor,
     width: lineWidth,
@@ -93,7 +93,7 @@ function draw(){
     background(backgroundColor);
 
     balls.forEach((ball) => {
-        moveBall(ball);
+        updateBall(ball);
         displayBall(ball);
     })
     drawLine(leftEdge);
@@ -101,21 +101,44 @@ function draw(){
 }
 
 
-const moveBall = (ball) => {
-    ball.x += ball.speed;
+function updateBall(ball){
+    console.log(ball.x);
+    if(ball.x + ball.size/2 > rightEdge.x1 ){
+        ball.speed *= -1;
+        activateLine(rightEdge);
+    } else if(ball.x - ball.size/2 < leftEdge.x1 ){
+        ball.speed *= -1;
+        activateLine(leftEdge);
+    }
+    ball.x+= ball.speed;
 }
 
 
-const displayBall = ({x, y, r, strokeColor, fillColor}) => {
+const displayBall = ({x, y, size, strokeColor, fillColor, ballStrokeWeight}) => {
         stroke(strokeColor);
-        fill(fillColor)
-        ellipse(x, y, r);
+        fill(fillColor);
+        strokeWeight(ballStrokeWeight);
+        ellipse(x, y, size);
 }
-
-
 
 function drawLine({x1, y1, x2, y2, color, width}){
     stroke(color);
     strokeWeight(width);
     line(x1, y1, x2, y2);
+}
+
+
+
+function activateLine(line){
+
+    line.color = activeLineColor;
+    line.width = activelineWidth;
+
+    setTimeout(() => resetLines(line), 500);
+}
+
+
+function resetLines(line){
+    line.color = lineColor;
+    line.width = lineWidth;
 }
